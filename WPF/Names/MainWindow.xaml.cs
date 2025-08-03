03ca892c00d56a18a4488ab856e4839faf09ab7e
mrandreyofficial <mrandreyofficial@gmail.com>
2025-08-03 12:25:35 +0500

    Names

---
+++ WPF/Names/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows;
+
+namespace Names;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void AddNameToListBox(object sender, RoutedEventArgs e)
+    {
+        var name = nameBox.Text;
+        var canAddName = !string.IsNullOrWhiteSpace(name)
+            && !listOfNames.Items.Contains(name);
+        
+        if (canAddName)
+        {
+            listOfNames.Items.Add(name);
+            nameBox.Clear();
+        }
+    }
+
+    private void RemoveName(object sender, RoutedEventArgs e)
+    {
+        var item = listOfNames.SelectedItem;
+
+        listOfNames.Items.Remove(item);
+    }
+
+    private void ClearBox(object sender, RoutedEventArgs e) => nameBox.Clear();
+}
---
+++ WPF/Names/Readme.md
@@ -0,0 +1,8 @@
+Mini app made in [WPF](https://learn.microsoft.com/en-us/dotnet/desktop/wpf/)
+
+### You can:
+1. Add name to list box
+2. Remove name from list box
+
+### Screenshots
+![app](./Screenshots/app.png)
